2da83df137cb4fefd7920811384b24d86318247e
刘博文 <1205404884@qq.com>
2026-01-22 21:09:52 +0800

    ci(deploy): 更新部署配置和工作流
    - 修改 GitHub Pages 部署工作流名称
    - 删除旧的 nextjs.yml 工作流文件
    - 更新 package.json 中的启动命令
    - 更新 Next.js 构建和缓存相关文件

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-df9508188de4c63c.js","static/chunks/fd9d1056-2fce5ef1b8d9778d.js","static/chunks/117-f1223021718c7000.js","static/chunks/main-app-50c805ab90189cc6.js"],pages:{"/_app":["static/chunks/webpack-df9508188de4c63c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-11b6a30f3ce07da4.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-df9508188de4c63c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-11b6a30f3ce07da4.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
